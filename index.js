b7917225193b33f8b5dfb9b2727e755d92621be1
Glenn Nguyo <glennnguyo1000@gmail.com>
2022-06-07 16:47:13 +0300

    Complete

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+let multiply
+multiply = num1*num2;
+
+let random
+random = Math.floor(Math.random()*10)
+
+const num3 = 10
+const num4 = 6
+let mod
+mod = num3%num4
+
+let max
+max = Math.max(1,2,3,4,5,20)
